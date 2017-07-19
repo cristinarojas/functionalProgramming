b65623f4d8e1f4f8f5a35ee9e31f636f091c1c1d
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-19 14:19:24 -0700

    Projecting arrays with forEach

--- projectingArrays.js
+++ projectingArrays.js
@@ -1,5 +1,7 @@
 // Applying a function to a value and creating a new value is called a projection. 
 // To project one array into another, we apply a projection function to each item in the array and collect the results in a new array.
+// 1.- Traverse the source array
+// 2.- Add each item's projected value to a new array
 (function() {
 	var newReleases = [
 		{
