5c2c08dfb725d6b9d17a60a52635a3cee6a72496
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-20 12:05:39 -0700

    Flatten and tree array with map and concat functions

--- queryingTreesconcatAll().js
+++ queryingTreesconcatAll().js
@@ -1,3 +1,64 @@
 //Trees pose a challenge because we need to flatten them 
 // into arrays in order to apply filter() and map() operations on them.
 // concatAll() function that we can combine with map() and filter() to query trees.
+
+// The concatAll() function iterates over each sub-array
+// in the array and collects the results in a new, flat array. Notice that 
+// the concatAll() function expects each item in the array to be another array.
+(function() {
+	var movieLists = [
+		{
+			name: "New Releases",
+			videos: [
+				{
+					"id": 70111470,
+					"title": "Die Hard",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 654356453,
+					"title": "Bad Boys",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		},
+		{
+			name: "Dramas",
+			videos: [
+				{
+					"id": 65432445,
+					"title": "The Chamber",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 4.0,
+					"bookmark": []
+				},
+				{
+					"id": 675465,
+					"title": "Fracture",
+					"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+					"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+					"rating": 5.0,
+					"bookmark": [{ id: 432534, time: 65876586 }]
+				}
+			]
+		}
+	];
+
+	return movieLists.map(function(itemsList) {
+		return itemsList.videos.map(function(item) {
+			return item.id;
+		});
+	}).concat();
+
+})();
+
+// Output
+/* [70111470, 654356453]
+   [65432445, 675465] */
