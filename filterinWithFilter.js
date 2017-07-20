98d2b109c01e6757689dfbf7f98290f3c4723011
Cristina Rojas Zamora <criss@Cristinas-MacBook-Pro-2.local>
2017-07-20 11:18:32 -0700

    Filtering Array with filter and collecting with map

---
+++ filterinWithFilter.js
@@ -0,0 +1,50 @@
+// To make filtering easier, let's add a filter() function to the Array type.
+// The filter() function accepts a predicate.
+// A predicate is a function that accepts an item in the array, 
+// and returns a boolean indicating whether the item should be retained in the new array.
+// Like map(), filter() lets us express what data we want without requiring us to specify how we want to collect the data.
+
+// Chain filter and map to collect the ids of videos that have a rating of 5.0
+(function() {
+	var newReleases = [
+		{
+			"id": 70111470,
+			"title": "Die Hard",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 654356453,
+			"title": "Bad Boys",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		},
+		{
+			"id": 65432445,
+			"title": "The Chamber",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 4.0,
+			"bookmark": []
+		},
+		{
+			"id": 675465,
+			"title": "Fracture",
+			"boxart": "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+			"uri": "http://api.netflix.com/catalog/titles/movies/70111470",
+			"rating": 5.0,
+			"bookmark": [{ id: 432534, time: 65876586 }]
+		}
+	];
+
+	return newReleases.filter(function(item) {
+		return item.rating === 5.0; // returns a boolean indicating whether the item should be retained in the new array
+	}).map(function(item) {
+		return item.id;
+	});
+
+})();
